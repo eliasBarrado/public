16e58af2ae60d2a1369a5ee10d38e44966df4811
eliasbar <ebarjim@gmail.com>
2018-12-14 18:29:57 +0100

    First commit in branch dev

--- public.py
+++ public.py
@@ -3,7 +3,6 @@ import time
 def getServerTime(k, unixtime=True):
 	try:
 		response = k.query_public('Time')
-
 	except Exception as error:
 		time.sleep(2)
 		getServerTime(k)
@@ -16,7 +15,6 @@ def getServerTime(k, unixtime=True):
 def getTickerInformation(k, pair):
 	try:
 		response = k.query_public('Ticker', {'pair': pair})
-
 	except Exception as error:
 		time.sleep(2)
 		getTickerInformation(k, pair)
@@ -26,7 +24,6 @@ def getTickerInformation(k, pair):
 def getOHLCData(k, pair, interval=1, since=None):
 	try:
 		response = k.query_public('OHLC', {'pair': pair, 'interval': interval, 'since': since})
-
 	except Exception as error:
 		time.sleep(2)
 		getOHLCData(k,pair,interval,since)
